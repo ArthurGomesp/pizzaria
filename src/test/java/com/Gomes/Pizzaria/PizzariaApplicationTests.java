21637b79505c0b6459c60f07b33281df9594d8a3
Arthur Gomes <ag0134486@gmail.com>
2023-12-26 19:46:19 -0300

    commit inicial

---
+++ src/test/java/com/Gomes/Pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gomes.Pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
